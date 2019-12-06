137311272ba283565fb1243b418bfd8a51a7fbbe
shuai1998 <2410461976@qq.com>
2019-12-06 09:29:35 +0800

    test

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+/**
+ * @author BaoShuai
+ * @Date 2019/12/4
+ */
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/Login.java
@@ -0,0 +1,9 @@
+/**
+ * @author BaoShuai
+ * @Date 2019/12/6
+ */
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("login success!");
+    }
+}
